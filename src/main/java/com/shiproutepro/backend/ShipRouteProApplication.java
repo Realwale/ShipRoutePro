93c5a72180f273e938a886735542e56660103a5e
Realwale <olawaleagboola41@gmail.com>
2024-07-24 17:32:23 +0100

    initialize project

---
+++ src/main/java/com/shiproutepro/backend/ShipRouteProApplication.java
@@ -0,0 +1,13 @@
+package com.shiproutepro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipRouteProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipRouteProApplication.class, args);
+	}
+
+}
